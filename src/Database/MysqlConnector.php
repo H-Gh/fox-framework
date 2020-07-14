b6dd73724acbffd6402c6fe133cb4209ae4f62e3
Hamed Ghasempour <business.hamedghasempour@gmail.com>
2020-07-14 22:42:54 +0400

    Add mysql connector to connect to mysql database

---
+++ src/Database/MysqlConnector.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Fox\Database;
+
+use PDO;
+
+/**
+ * The mysql connection to a mysql database
+ * PHP version >= 7.4
+ *
+ * @category Database
+ * @package  Fox
+ * @author   Hamed Ghasempour <hamedghasempour@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     null
+ */
+class MysqlConnector implements ConnectorInterface
+{
+
+    /**
+     * Connect to database
+     *
+     * @return mixed
+     */
+    public function connect()
+    {
+        return new MysqlConnection(
+            new PDO(
+                'mysql:host=' . getenv("MYSQL_HOST") . ';dbname=' . getenv("MYSQL_DATABASE"),
+                getenv("MYSQL_USERNAME"),
+                getenv("MYSQL_PASSWORD")
+            )
+        );
+    }
+}
