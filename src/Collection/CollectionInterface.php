cfb6fd9a7f1c79ac5d60130efd550e66b48d3de0
Hamed Ghasempour <business.hamedghasempour@gmail.com>
2020-07-14 22:38:42 +0400

    Add a interface to define all the methods that collection will have

---
+++ src/Collection/CollectionInterface.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Fox\Collection;
+
+/**
+ * Interface to Collection concept
+ * PHP version >= 7.0
+ *
+ * @category Collection
+ * @package  Fox
+ * @author   Hamed Ghasempour <hamedghasempour@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     null
+ */
+interface CollectionInterface
+{
+    /**
+     * Put new data into array
+     *
+     * @param mixed $data  The data
+     * @param null  $index The index of array
+     *
+     * @return CollectionInterface
+     */
+    public function add($data, $index = null): CollectionInterface;
+
+    /**
+     * Get the data from array
+     *
+     * @param int $index The index of array
+     *
+     * @return mixed
+     */
+    public function get(int $index);
+
+    /**
+     * Update and existence index of array
+     *
+     * @param int   $index The index of array
+     * @param mixed $data  The data
+     *
+     * @return CollectionInterface
+     */
+    public function update(int $index, $data): CollectionInterface;
+}
