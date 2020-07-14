e18aa97b4e1ceeb02efb8907ca737edb710497f3
Hamed Ghasempour <business.hamedghasempour@gmail.com>
2020-07-14 23:04:29 +0400

    Add a connection factory to create the connector object based on the the value from the env file

---
+++ src/Database/ConnectorFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Fox\Database;
+
+use Fox\Exception\NotFoundException;
+
+/**
+ * The factory to create a connector to a database
+ * PHP version php >= 7.0
+ *
+ * @category Database
+ * @author   Hamed Ghasempour <hamedghasempour@gmail.com>
+ */
+class ConnectorFactory
+{
+    /**
+     * @param string $databaseName The database name, like mysql or mongo
+     *
+     * @return ConnectorInterface
+     * @throws NotFoundException
+     */
+    public static function create($databaseName = null)
+    {
+        $namespace = "Fox\Database\\" . ucfirst(strtolower($databaseName)) . "Connector";
+        if(class_exists($namespace)) {
+            return new $namespace();
+        }
+        throw new NotFoundException("The connector was not found.");
+    }
+}
